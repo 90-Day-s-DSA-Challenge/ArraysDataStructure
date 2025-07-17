15bbc1631830a0a2afb8934409f0808499b15530
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-07-17 19:20:16 +0530

    two sum optimise, leetcode, easy, array

--- ArrayEasyProblems/src/main/java/org/example/TwoSum.java
+++ ArrayEasyProblems/src/main/java/org/example/TwoSum.java
@@ -4,18 +4,40 @@ package org.example;
 //https://leetcode.com/problems/two-sum/description/
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
+        int[] nums = {3,2,4};
+        int target = 6;
 
-        int[] ans = twoSum(nums, target);
+        int[] ans = twoSum2(nums, target);
         System.out.println(Arrays.toString(ans));
 
 
     }
+    public static int[] twoSum2(int[] nums, int target){
+        HashMap<Integer, Integer> map = new HashMap<>(); // value -> index
 
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+
+            System.out.println(complement);
+            System.out.println(map.containsKey(complement));
+
+            // Check if the complement already exists
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            // Store current number and its index
+            map.put(nums[i], i);
+        }
+
+        // If no solution is found
+        return new int[] { -1, -1 };
+    }
     public static int[] twoSum(int[] nums, int target){
         int n = nums.length;
         int[] result = {-1, -1};
@@ -31,6 +53,7 @@ public class TwoSum {
 
         return result;
     }
+
 
 
 }
